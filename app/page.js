707b331e149d4de4adf1244a1eb669743d48b978
Luiz Claudio <luizclaudioarruda@outlook.com>
2023-12-15 18:54:00 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           ><span class="relative flex h-3 w-3">
-  <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-500 opacity-75"></span>
+  <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-300 opacity-95"></span>
   <span class="relative inline-flex rounded-full h-3 w-3 bg-blue-700"></span>
 </span>
             COMEÇE A EXPLORAR{' '}
